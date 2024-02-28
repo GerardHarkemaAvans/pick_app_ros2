6ae34908f0943eb7a4dc92895c07cae73d4f6ca8
Gerard Harkema <GA.Harkema@avans.nl>
2024-02-28 16:54:58 +0100

    20240228

--- pick_n_drop/include/pick_n_drop/object_detection_class.hpp
+++ pick_n_drop/include/pick_n_drop/object_detection_class.hpp
@@ -9,21 +9,19 @@
 #include "rclcpp/node.hpp"
 #include "std_msgs/msg/string.hpp"
 using std::placeholders::_1;
-#include "sensor_msgs/msg/point_cloud2.hpp"
-#include "sensor_msgs/msg/image.hpp"
 #include "depthai_ros_msgs/msg/spatial_detection_array.hpp"
+#include "depthai_ros_msgs/msg/spatial_detection.hpp"
 //#include <tf2_geometry_msgs/tf2_geometry_msgs.h>
 #include "geometry_msgs/msg/point.hpp"
+#include "geometry_msgs/msg/transform_stamped.hpp"
+#include "tf2_ros/transform_broadcaster.h"
 
 
 
-#include "tf2/exceptions.h"
-#include "tf2_ros/transform_listener.h"
-#include "tf2_ros/buffer.h"
+//#include "tf2/exceptions.h"
+//#include "tf2_ros/transform_listener.h"
+//#include "tf2_ros/buffer.h"
 
-//#include <pcl/common/transforms.h>
-//#include <pcl/point_types.h>
-//#include <pcl/point_cloud.h>
 
 using namespace std::chrono_literals;
 
@@ -34,13 +32,13 @@ class ObjectDetectionClass
   std::shared_ptr<rclcpp::Node> _node;
   public:
     ObjectDetectionClass(std::shared_ptr<rclcpp::Node> node);
-    geometry_msgs::msg::Point getNearestObjectPosition();
+    geometry_msgs::msg::TransformStamped getNearestObjectPosition();
   private:
 
     rclcpp::Subscription<depthai_ros_msgs::msg::SpatialDetectionArray>::SharedPtr detections_subscription_;
     void detections_callback(depthai_ros_msgs::msg::SpatialDetectionArray::SharedPtr msg) const;
-    depthai_ros_msgs::msg::SpatialDetectionArray::SharedPtr detections;
-    mutable  depthai_ros_msgs::msg::SpatialDetectionArray::SharedPtr detections_ = nullptr;
+    mutable  depthai_ros_msgs::msg::SpatialDetectionArray::SharedPtr detections_array = nullptr;
+    std::unique_ptr<tf2_ros::TransformBroadcaster> tf_broadcaster;
 };
 
 #endif
--- pick_n_drop/src/object_detection_class.cpp
+++ pick_n_drop/src/object_detection_class.cpp
@@ -1,7 +1,4 @@
-#include <memory>
-
 #include "rclcpp/rclcpp.hpp"
-#include <string.h>
 using std::placeholders::_1;
 
 #include "pick_n_drop/object_detection_class.hpp"
@@ -12,29 +9,50 @@ ObjectDetectionClass::ObjectDetectionClass(std::shared_ptr<rclcpp::Node> node) :
       printf("ObjectDetectionClass constructor\n");
 
       detections_subscription_ = node->create_subscription<depthai_ros_msgs::msg::SpatialDetectionArray>("DummyDetections", 10, std::bind(&ObjectDetectionClass::detections_callback, this, std::placeholders::_1));
-
+      //tf_broadcaster = std::make_unique<tf2_ros::TransformBroadcaster>(*this);
 }
 
 
 void ObjectDetectionClass::detections_callback(depthai_ros_msgs::msg::SpatialDetectionArray::SharedPtr msg) const
 {
-//      detections = std::make_shared<depthai_ros_msgs::msg::SpatialDetectionArray_<std::allocator<void>>>(msg);
-
-      //detections = &msg;
-      RCLCPP_INFO(_node->get_logger(), "I received the message , ID: %s", msg->header.frame_id.c_str());
-      //printf("Received message Id: %s\n", msg->header.frame_id.c_str());
-      detections_ = msg;
-
+      detections_array = msg;
 }
 
-geometry_msgs::msg::Point ObjectDetectionClass::getNearestObjectPosition(){
-      geometry_msgs::msg::Point pnt;
+geometry_msgs::msg::TransformStamped ObjectDetectionClass::getNearestObjectPosition(){
+      geometry_msgs::msg::TransformStamped transform;
 
-      if(detections_){
-            RCLCPP_INFO(_node->get_logger(), "I current the message , ID: %s", detections_->header.frame_id.c_str());  
-            detections_ = nullptr;
+      rclcpp::Time t = _node->now();
+
+      if(detections_array){
+            //RCLCPP_INFO(_node->get_logger(), "I current the message , ID: %s", detections_array->header.frame_id.c_str());  
+            std::vector<depthai_ros_msgs::msg::SpatialDetection>::iterator it;
+            std::vector<depthai_ros_msgs::msg::SpatialDetection>::iterator nearest_detection = detections_array->detections.begin();
+            for (it = detections_array->detections.begin(); it != detections_array->detections.end(); ++it){
+                  if(it->position.z < nearest_detection->position.z){
+                        nearest_detection = it;
+                  }
+                  //RCLCPP_INFO(_node->get_logger(), "I current z position: %f", it->position.z);
+            }
+            RCLCPP_INFO(_node->get_logger(), "Nearest z position: %f",nearest_detection->position.z);
+
+            transform.header.stamp = _node->now();
+            transform.header.frame_id = "oak_rgb_camera_optical_frame";
+            transform.child_frame_id = "nearest_object";
+
+            transform.transform.translation.x = nearest_detection->position.x;
+            transform.transform.translation.y = -nearest_detection->position.y;
+            transform.transform.translation.z = nearest_detection->position.z;
+
+            transform.transform.rotation.x = 0.0;
+            transform.transform.rotation.y = 0.0;
+            transform.transform.rotation.z = 0.0;
+            transform.transform.rotation.w = 1.0;
+
+            detections_array = nullptr;
+            //tf_broadcaster->sendTransform(transform);
+
       }    
 
-      return pnt;
+      return transform;
       
 }
